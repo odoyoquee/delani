60db1c08310f38446ab605dce049c71625255343
odoyoquee <odoyograce23@gmail.com>
2020-02-20 16:06:02 +0300

    added a what we do section

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(".OnCrack1").click(function() {
+        $("#click1").toggle();
+        $("#magic1").toggle();
+    });
+    $(".OnCrack2").click(function() {
+        $("#magic2").toggle();
+        $("#click2").toggle();
+    });
+    $(".OnCrack3").click(function() {
+        $("#magic3").toggle();
+        $("#click3").toggle();
+    });
+});
