7d1b033a6258e392ff618789dfd3aaa0a715be89
odoyoquee <odoyograce23@gmail.com>
2020-02-21 13:58:12 +0300

    added a footer

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-$(document).ready(function() {
-    $(".onCrack1").click(function() {
+$(document).ready(function () {
+    $(".onCrack1").click(function () {
         $("#click1").toggle();
         $("#magic1").toggle();
     });
-    
-    $(".onCrack2").click(function() {
+
+    $(".onCrack2").click(function () {
         $("#magic2").toggle();
         $("#click2").toggle();
     });
-    $(".onCrack3").click(function() {
+    $(".onCrack3").click(function () {
         $("#magic3").toggle();
         $("#click3").toggle();
     });
 });
+
+function alert() {
+    alert('this works');
+}
+
 // var btn = document.getElementById("myBtn");
 
 // // When the user clicks on the button, open the modal
@@ -41,27 +46,24 @@ $(document).ready(function() {
 // }
 
 
-function alert(){
-    alert('this works');
-}
 
-function valid(){
+function valid() {
 
-     var x = document.forms["myForm"]["contact-name"].value;
-     if (x == "") {
-         alert("Name not entered");
-         return false;
-     }
-     var y = document.forms["myForm"]["contact-email"].value;
-     if (y == "") {
-         alert("Email not entered");
-         return false;
-     }
-     var z = document.forms["myForm"]["contact-message"].value;
-     if (z == "") {
-         alert("You have no message to send");
-         return false
-             }
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false
+    }
     var person1Input = $("input#contact-name").val();
     $("span#outputName").append(person1Input);
     $('.toast').toast('show');
